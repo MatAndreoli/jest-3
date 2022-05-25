f2acae55d7da84177302f5bfbe0bfb5030a9d453
MatAndreoli <andreoli.2003@gmail.com>
2022-05-25 11:53:23 -0300

    fixed exercise2 tests

--- spec/exercise2.spec.js
+++ spec/exercise2.spec.js
@@ -1,36 +1,36 @@
 const exercise2 = require('../src/exercise2');
 
 describe('Given exercise2', () => {
-    describe('When isNumber2 is called with 1', () => {
-        let result;
-        beforeEach(() => {
-            result = exercise2.isNumber2(1);
-        });
-
-        it('Then should return false', () => {
-            expect(result).toBeFalsy();
-        });
+  describe('When mainMethod is called', () => {
+    beforeEach(() => {
+      jest.spyOn(exercise2, 'isNumber2');
+      exercise2.mainMethod();
     });
 
-    describe('When isNumber2 is called with 2', () => {
-        let result;
-        beforeEach(() => {
-            result = exercise2.isNumber2(2);
-        });
+    it('Then should call isNumber2', () => {
+      expect(exercise2.isNumber2).toHaveBeenCalledWith(3);
+    });
+  });
 
-        it('Then should return true', () => {
-            expect(result).toBeTruthy()
-        });
+  describe('When isNumber2 is called with 1', () => {
+    let result;
+    beforeEach(() => {
+      result = exercise2.isNumber2(1);
     });
 
-    describe('When mainMethod is called', () => {
-        beforeEach(() => {
-            exercise2.isNumber2 = jest.fn(() => { return false });
-            exercise2.mainMethod();
-        });
-    
-        it('Then should call isNumber2', () => {
-            expect(exercise2.isNumber2).toHaveBeenCalledWith(3);
-        });
+    it('Then should return false', () => {
+      expect(result).toBeFalsy();
     });
+  });
+
+  describe('When isNumber2 is called with 2', () => {
+    let result;
+    beforeEach(() => {
+      result = exercise2.isNumber2(2);
+    });
+
+    it('Then should return true', () => {
+      expect(result).toBeTruthy();
+    });
+  });
 });
